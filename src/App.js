f520d497eae1a33d25500215d8a9ce7f830bfb58
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-05-04 19:05:32 +1200

    Adding search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 import React from "react";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <header>
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project was coded by Sophie Joe and is{" "}
+          <a
+            href="https://github.com/sophie-joe/react-dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function handleKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a word..."
+              className="form-control"
+              autoFocus="on"
+              onChange={handleKeyword}
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
