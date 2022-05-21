d9351e9282489d237bf7183ec8bc564fadb7c00c
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-05-21 21:31:48 +1200

    Adding photos for search terms

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,16 +2,33 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("sunset");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
   function search() {
     //docs => https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000014a1f5828fb764207b8ce13874e32c8cd";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+
+    //docs => https://www.pexels.com/api/documentation/?
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -61,6 +78,7 @@ export default function Dictionary() {
         </div>
         <section>
           <Results results={results} />
+          <Photos photos={photos} />
         </section>
       </div>
     );
